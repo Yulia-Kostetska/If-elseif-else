a7bf4486649ef6e66c3e92dd77b537a055944a2d
Yulia Kostetska <julia.kostetska@gmail.com>
2023-07-04 11:08:07 +0300

    Example data type

---
+++ index.js
@@ -0,0 +1,17 @@
+console.log(undefined); 
+console.log(null);
+
+/*boolean*/
+console.log(true);
+console.log(false);
+
+/*string*/
+console.log('true');
+console.log("false");
+console.log("I'm a teacher");
+
+console.log(65654556);
+console.log(256.3265);
+console.log(-Infinity);
+console.log(NaN);
+console.log(1n);
